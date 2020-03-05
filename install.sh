c037e3fbecd9ad58171e2a602eaba4787a7d9508
Sam <sam.w.neal@gmail.com>
2020-03-05 11:03:41 +1100

    fix debian 9 support typo

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ then
     curl -O https://repo.zabbix.com/zabbix/4.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.4-1+bionic_all.deb
     dpkg -i zabbix-release_4.4-1+bionic_all.deb
 elif [ $(lsb_release -cs) = "stretch" ]
+then
     curl -O http://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/zabbix-release_4.4-1+stretch_all.deb
     dpkg -i zabbix-release_4.4-1+stretch_all.deb
 else
