b433c9f903f61b7d1a47f9d891174acdf614da7e
Sam <sam.w.neal@gmail.com>
2020-03-05 11:02:51 +1100

    Add Debian 9 (stretch) support

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ elif [ $(lsb_release -cs) = "bionic" ]
 then
     curl -O https://repo.zabbix.com/zabbix/4.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.4-1+bionic_all.deb
     dpkg -i zabbix-release_4.4-1+bionic_all.deb
+elif [ $(lsb_release -cs) = "stretch" ]
+    curl -O http://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/zabbix-release_4.4-1+stretch_all.deb
+    dpkg -i zabbix-release_4.4-1+stretch_all.deb
 else
     echo "Non-compatible release"
     exit
