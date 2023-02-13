20926ed5a4a7cf8536ada68e9d646a7f7f394031
Sam <sam.w.neal@gmail.com>
2023-02-13 12:38:51 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,10 @@ elif [ $(lsb_release -cs) = "focal" ]
 then
     curl -O https://repo.zabbix.com/zabbix/5.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.2-1+ubuntu20.04_all.deb
     dpkg -i zabbix-release_5.2-1+ubuntu20.04_all.deb
+elif
+then [ $(lsb_release -cs) = "jammy" ]
+    curl -O https://repo.zabbix.com/zabbix/5.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.2-1+ubuntu20.04_all.deb
+    dpkg -i zabbix-release_5.2-1+ubuntu20.04_all.deb
 else
     echo "Non-compatible release"
     exit
