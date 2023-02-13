a198d8f8a1f56335aca75ac636ab5a241f1051bb
Sam <sam.w.neal@gmail.com>
2023-02-13 12:42:15 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ elif [ $(lsb_release -cs) = "focal" ]
 then
     curl -O https://repo.zabbix.com/zabbix/5.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.2-1+ubuntu20.04_all.deb
     dpkg -i zabbix-release_5.2-1+ubuntu20.04_all.deb
-elif
-then [ $(lsb_release -cs) = "jammy" ]
+elif [ $(lsb_release -cs) = "jammy" ]
+then
     curl -O https://repo.zabbix.com/zabbix/5.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.2-1+ubuntu20.04_all.deb
     dpkg -i zabbix-release_5.2-1+ubuntu20.04_all.deb
 else
